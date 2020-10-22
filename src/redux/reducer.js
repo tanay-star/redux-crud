24064bc099bfb2daea04fd2c50730b515c56d602
Tanay Banerjee <banerjeetanay333@gmail.com>
2020-10-22 18:32:35 +0530

    updated

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,6 +1,6 @@
 const initialstate = {
   employees: [
-    { id: 1, employeeName: 'Employee 1', employeeDepartment: '.NET Team' },
+    { id: 1, employeeName: 'Employee 1', employeeDepartment: 'Web Team' },
     { id: 2, employeeName: 'Employee 2', employeeDepartment: 'Mobile Team' },
     { id: 3, employeeName: 'Employee 3', employeeDepartment: 'Design Team' },
   ],
